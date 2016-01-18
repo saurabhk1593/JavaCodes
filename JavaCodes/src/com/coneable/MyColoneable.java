6a0bc69ecacec9c3f58cf9ce8dc7116cc87d47a7
saurabhk1593 <saurabhk1593@gmail.com>
2016-01-18 23:01:46 +0530

    aer

---
+++ JavaCodes/src/com/coneable/MyColoneable.java
@@ -0,0 +1,13 @@
+package com.coneable;
+
+import com.coneable.Box;
+/**
+ * Created by Saurabh on 1/18/2016.
+ */
+public class MyColoneable {
+    public static void main(){
+        Box b = new Box(1,2,3);
+        System.out.println(b);
+    }
+
+}
--- JavaCodes/src/com/saurabh/Driver.java
+++ JavaCodes/src/com/saurabh/Driver.java
@@ -2,11 +2,13 @@ package com.saurabh;
 
 
 import com.collection.MyList;
+import com.coneable.MyColoneable;
 import com.serializable.MySerializable;
 
 public class Driver {
     public static void main(String args[]){
         //MyList.main();
-        MySerializable.main();
+       // MySerializable.main();
+        MyColoneable.main();
     }
 }
